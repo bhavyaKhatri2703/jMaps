d19b0738cfc0a0dfe27e917baca63f07b9b9e552
bhavyakhatri2703 <khatribhavya2703@gmail.com>
2024-11-24 22:26:15 +0530

    Modified

--- main.cpp
+++ main.cpp
@@ -114,6 +114,352 @@ int main()
         drawNodes(window, points, MAX_NODES, dot, font);
         drawEdges(window, edges, points);
 
+
+    sf::Texture texture;
+    if (!texture.loadFromFile("abb3.png")) {
+        std::cerr << "Error loading image" << std::endl;
+        return -1;
+    }
+
+    // Create a sprite and set its texture
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+
+    // Set the position of the sprite (e.g., x=100, y=150)
+    sprite.setPosition(100.f, 100.f);
+
+    // Set the size of the sprite (e.g., width=200, height=150)
+    float targetWidth = 100.f;
+    float targetHeight = 350.f;
+
+    // Calculate the scale factors based on the texture's original size
+    sf::Vector2u textureSize = texture.getSize();
+    float scaleX = targetWidth / textureSize.x;
+    float scaleY = targetHeight / textureSize.y;
+
+    // Apply the scale to the sprite
+    sprite.setScale(scaleX, scaleY);
+
+         window.draw(sprite);
+
+
+    sf::Texture texture1;
+    if (!texture1.loadFromFile("audi.png")) {
+        std::cerr << "Error loading image" << std::endl;
+        return -1;
+    }
+
+    // Create a sprite and set its texture
+    sf::Sprite sprite1;
+    sprite1.setTexture(texture1);
+
+    // Set the position of the sprite (e.g., x=100, y=150)
+    sprite1.setPosition(230.f, 110.f);
+
+    // Set the size of the sprite (e.g., width=200, height=150)
+     targetWidth = 200.f;
+     targetHeight = 300.f;
+
+    // Calculate the scale factors based on the texture's original size
+    textureSize = texture.getSize();
+    scaleX = targetWidth / textureSize.x;
+    scaleY = targetHeight / textureSize.y;
+
+    // Apply the scale to the sprite
+    sprite1.setScale(scaleX, scaleY);
+
+         window.draw(sprite1);
+
+         
+         
+         sf::Texture texture2;
+    if (!texture2.loadFromFile("abb1.png")) {
+        std::cerr << "Error loading image" << std::endl;
+        return -1;
+    }
+
+    // Create a sprite and set its texture
+    sf::Sprite sprite2;
+    sprite2.setTexture(texture2);
+
+    // Set the position of the sprite (e.g., x=100, y=150)
+    sprite2.setPosition(360.f, 90.f);
+
+    // Set the size of the sprite (e.g., width=200, height=150)
+     targetWidth = 160.f;
+     targetHeight = 390.f;
+
+    // Calculate the scale factors based on the texture's original size
+    textureSize = texture2.getSize();
+    scaleX = targetWidth / textureSize.x;
+    scaleY = targetHeight / textureSize.y;
+
+    // Apply the scale to the sprite
+    sprite2.setScale(scaleX, scaleY);
+
+         window.draw(sprite2);
+         
+         sf::Texture texture3;
+    if (!texture3.loadFromFile("grass.jpg")) {
+        std::cerr << "Error loading image" << std::endl;
+        return -1;
+    }
+
+    // Create a sprite and set its texture
+    sf::Sprite sprite3;
+    sprite3.setTexture(texture3);
+
+    // Set the position of the sprite (e.g., x=100, y=150)
+    sprite3.setPosition(230.f, 210.f);
+
+    // Set the size of the sprite (e.g., width=200, height=150)
+     targetWidth = 120.f;
+     targetHeight = 140.f;
+
+    // Calculate the scale factors based on the texture's original size
+    textureSize = texture3.getSize();
+    scaleX = targetWidth / textureSize.x;
+    scaleY = targetHeight / textureSize.y;
+
+    // Apply the scale to the sprite
+    sprite3.setScale(scaleX, scaleY);
+
+         window.draw(sprite3);
+
+         sf::Texture texture4;
+    if (!texture4.loadFromFile("jbs1.png")) {
+        std::cerr << "Error loading image" << std::endl;
+        return -1;
+    }
+
+    // Create a sprite and set its texture
+    sf::Sprite sprite4;
+    sprite4.setTexture(texture4);
+
+    // Set the position of the sprite (e.g., x=100, y=150)
+    sprite4.setPosition(90.f, 610.f);
+
+    // Set the size of the sprite (e.g., width=200, height=150)
+     targetWidth = 160.f;
+     targetHeight = 100.f;
+
+    // Calculate the scale factors based on the texture's original size
+    textureSize = texture4 .getSize();
+    scaleX = targetWidth / textureSize.x;
+    scaleY = targetHeight / textureSize.y;
+
+    // Apply the scale to the sprite
+    sprite4.setScale(scaleX, scaleY);
+
+    window.draw(sprite4);
+
+         
+         
+
+
+
+          sf::Texture texture5;
+    if (!texture5.loadFromFile("sarojni.png")) {
+        std::cerr << "Error loading image" << std::endl;
+        return -1;
+    }
+
+    // Create a sprite and set its texture
+    sf::Sprite sprite5;
+    sprite5.setTexture(texture5);
+
+    // Set the position of the sprite (e.g., x=100, y=150)
+    sprite5.setPosition(360.f, 610.f);
+
+    // Set the size of the sprite (e.g., width=200, height=150)
+     targetWidth = 160.f;
+     targetHeight = 100.f;
+
+    // Calculate the scale factors based on the texture's original size
+    textureSize = texture5 .getSize();
+    scaleX = targetWidth / textureSize.x;
+    scaleY = targetHeight / textureSize.y;
+
+    // Apply the scale to the sprite
+    sprite5.setScale(scaleX, scaleY);
+
+    window.draw(sprite5);
+
+    
+
+    sf::Texture texture6;
+    if (!texture6.loadFromFile("mandir.png")) {
+        std::cerr << "Error loading image" << std::endl;
+        return -1;
+    }
+
+    // Create a sprite and set its texture
+    sf::Sprite sprite6;
+    sprite6.setTexture(texture6);
+
+    // Set the position of the sprite (e.g., x=100, y=150)
+    sprite6.setPosition(430.f, 490.f);
+
+    // Set the size of the sprite (e.g., width=200, height=150)
+     targetWidth = 90.f;
+     targetHeight = 90.f;
+
+    // Calculate the scale factors based on the texture's original size
+    textureSize = texture6 .getSize();
+    scaleX = targetWidth / textureSize.x;
+    scaleY = targetHeight / textureSize.y;
+
+    // Apply the scale to the sprite
+    sprite6.setScale(scaleX, scaleY);
+
+    window.draw(sprite6);
+
+
+
+    sf::Texture texture7;
+    if (!texture7.loadFromFile("annapurna.png")) {
+        std::cerr << "Error loading image" << std::endl;
+        return -1;
+    }
+
+    // Create a sprite and set its texture
+    sf::Sprite sprite7;
+    sprite7.setTexture(texture7);
+
+    // Set the position of the sprite (e.g., x=100, y=150)
+    sprite7.setPosition(570.f, 610.f);
+
+    // Set the size of the sprite (e.g., width=200, height=150)
+     targetWidth = 280.f;
+     targetHeight = 100.f;
+
+    // Calculate the scale factors based on the texture's original size
+    textureSize = texture7 .getSize();
+    scaleX = targetWidth / textureSize.x;
+    scaleY = targetHeight / textureSize.y;
+
+    // Apply the scale to the sprite
+    sprite7.setScale(scaleX, scaleY);
+
+    window.draw(sprite7);
+
+
+
+
+    sf::Texture texture8;
+    if (!texture8.loadFromFile("bask.png")) {
+        std::cerr << "Error loading image" << std::endl;
+        return -1;
+    }
+
+    // Create a sprite and set its texture
+    sf::Sprite sprite8;
+    sprite8.setTexture(texture8);
+
+    // Set the position of the sprite (e.g., x=100, y=150)
+    sprite8.setPosition(235.f, 420.f);
+
+    // Set the size of the sprite (e.g., width=200, height=150)
+     targetWidth = 100.f;
+     targetHeight = 160.f;
+
+    // Calculate the scale factors based on the texture's original size
+    textureSize = texture8 .getSize();
+    scaleX = targetWidth / textureSize.x;
+    scaleY = targetHeight / textureSize.y;
+
+    // Apply the scale to the sprite
+    sprite8.setScale(scaleX, scaleY);
+
+    window.draw(sprite8);
+
+
+
+
+    sf::Texture texture9;
+    if (!texture9.loadFromFile("hostel.png")) {
+        std::cerr << "Error loading image" << std::endl;
+        return -1;
+    }
+
+    // Create a sprite and set its texture
+    sf::Sprite sprite9;
+    sprite9.setTexture(texture9);
+
+    // Set the position of the sprite (e.g., x=100, y=150)
+    sprite9.setPosition(900.f, 470.f);
+    
+
+    // Set the size of the sprite (e.g., width=200, height=150)
+     targetWidth = 100.f;
+     targetHeight = 100.f;
+
+    // Calculate the scale factors based on the texture's original size
+    textureSize = texture9 .getSize();
+    scaleX = targetWidth / textureSize.x;
+    scaleY = targetHeight / textureSize.y;
+
+    // Apply the scale to the sprite
+    sprite9.setScale(scaleX, scaleY);
+
+    window.draw(sprite9);
+
+
+
+
+     sf::Sprite sprite10;
+    sprite10.setTexture(texture9);
+
+    // Set the position of the sprite (e.g., x=100, y=150)
+    sprite10.setPosition(1060.f, 470.f);
+
+     sprite10.setScale(scaleX, scaleY);
+
+    window.draw(sprite10);
+
+
+
+    sf::Sprite sprite11;
+    sprite11.setTexture(texture9);
+
+    // Set the position of the sprite (e.g., x=100, y=150)
+    sprite11.setPosition(1060.f, 620.f);
+
+     sprite11.setScale(scaleX, scaleY);
+
+    window.draw(sprite11);
+
+
+
+
+
+  sf::Texture texture12;
+    if (!texture12.loadFromFile("faculty.png")) {
+        std::cerr << "Error loading image" << std::endl;
+        return -1;
+    }
+
+    // Create a sprite and set its texture
+    sf::Sprite sprite12;
+    sprite12.setTexture(texture12);
+
+    // Set the position of the sprite (e.g., x=100, y=150)
+    sprite12.setPosition(630.f, 470.f);
+
+    // Set the size of the sprite (e.g., width=200, height=150)
+     targetWidth = 180.f;
+     targetHeight = 100.f;
+
+    // Calculate the scale factors based on the texture's original size
+    textureSize = texture12 .getSize();
+    scaleX = targetWidth / textureSize.x;
+    scaleY = targetHeight / textureSize.y;
+
+    // Apply the scale to the sprite
+    sprite12.setScale(scaleX, scaleY);
+
+    window.draw(sprite12);
+
         // Render computed paths if an algorithm has been chosen
         if (algorithmChosen)
         {
